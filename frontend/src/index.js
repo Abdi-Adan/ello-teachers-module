d6d392e528f1fcb2874282d09b5e3bb8da50f38a
A. Adan <adanabdi036@gmail.com>
2024-06-12 14:07:14 +0300

    integrate graphql

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
@@ -8,9 +9,18 @@ import theme from './theme';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>,
+
 );
+
